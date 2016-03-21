e76591a06a8bf9bcdf2de8f7bfd0ad798da4f71c
John Slota <john.slota@gmail.com>
2016-03-21 13:37:15 -0600

    final portfolio

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::IdeasController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Idea.all
+  end
+
+  def show
+    respond_with Idea.find_by(id: params[:id])
+  end
+
+  def create
+    respond_with Idea.create(idea_params), location: nil
+  end
+
+  def update
+    idea = Idea.find_by(id: params[:id])
+    respond_with idea.update(idea_params)
+  end
+
+  def destroy
+    respond_with Idea.delete(params[:id])
+  end
+  
+  private
+
+  def idea_params
+    params.permit(:title, :body, :quality)
+  end
+end
---
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::IdeasControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
