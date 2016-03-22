dc5e46c702e36a6a4e023fa7974d63024b0b799f
John Slota <john.slota@gmail.com>
2016-03-22 16:55:59 -0600

    delete is fully functional

--- app/assets/javascripts/delete-idea.js
+++ app/assets/javascripts/delete-idea.js
@@ -1,6 +1,16 @@
 function deleteIdea(){
   $('.ideas-list').delegate('.delete-idea','click', function(){
     console.log("Im here")
-    
+    var $idea = $(this).closest(".idea")
+      $.ajax({
+      type: 'DELETE',
+      url: 'api/v1/ideas/'+ $idea.attr("data-id"),
+      success: function() {
+        $idea.remove()
+      },
+      error: function(xhr) {
+        console.log(xhr)
+      }
+    })
   })
 }
