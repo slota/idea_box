c26c26beb91d41dcafce6a5e8013aa49f211f738
John Slota <john.slota@gmail.com>
2016-03-23 13:19:35 -0600

    filtering is working as expected

---
+++ app/assets/javascripts/search-ideas.js
@@ -0,0 +1,10 @@
+function searchIdeas() {
+  $('#search').keyup(function() {
+    var $rows = $('.idea');
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+  })
+}
