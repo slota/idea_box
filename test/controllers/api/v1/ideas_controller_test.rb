ffaf3b96860e8403aa6a284f45fe21a61cff0c7f
John Slota <john.slota@gmail.com>
2016-03-24 09:07:10 -0600

    testing

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -1,7 +1,58 @@
-require 'test_helper'
+require "test_helper"
 
 class Api::V1::IdeasControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @idea1 = Idea.create(name: "john", description: "kewl")
+    @idea2 = Idea.create(name: "edgar", description: "sweet")
+  end
+
+  test "#index" do
+    get :index, format: :json
+
+    ideas = JSON.parse(response.body)
+    assert_response(:success)
+
+    assert_equal ideas.first["name"], "edgar"
+    assert_equal ideas.first["description"], "sweet"
+    assert_equal ideas.first["quality"], "swill"
+
+    assert_equal ideas.last["name"], "MyString"
+    assert_equal ideas.last["description"], "MyString"
+    assert_equal ideas.last["quality"], "MyString"
+
+
+  end
+
+  test "#show" do
+    get :show, format: :json, id: @idea2.id
+
+    idea = JSON.parse(response.body)
+    assert_response(:success)
+
+    assert_equal idea["name"], "edgar"
+    assert_equal idea["description"], "sweet"
+    assert_equal idea["quality"], "swill"
+  end
+
+  test "#delete" do
+    assert_equal Idea.count, 4
+    delete :destroy, format: :json, id: @idea2.id
+
+    assert_response(:success)
+    assert_equal Idea.count, 3
+  end
+
+  test "#create" do
+    post :create, format: :json, name: "steve", description: "super kewl", image_url: "tight"
+
+    idea = JSON.parse(response.body)
+    assert_response(:success)
+
+    assert_equal idea.count, 6
+    assert_equal idea["name"], "steve"
+    assert_equal idea["description"], "super kewl"
+    assert_equal idea["quality"], "swill"
+  end
+
 end
